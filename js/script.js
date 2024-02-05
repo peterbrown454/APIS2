a390f73fb5d3346a2da168ec9bec608f9d3d2995
Peter Brown <peterbrown454@gmail.com>
2024-02-05 15:55:01 +0000

    whatever

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 const API_KEY = "4ld1ksekhLhF3KlnMcyTezNeqFQ";
-const API_URL = "https://ci-jshint.herokuapp.com/api"
-const resultsModal = new bootstrap.modal(document.getElementById("resultsModal"))
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_URL}api_key=${API_KEY}`;
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
     const response = await fetch(queryString);
 
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        console.log(data.response);
+    } else {
+        console.log("error");
     }
 }
